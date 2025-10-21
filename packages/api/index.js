42631d1e8529d037e881347173b97d686b3edf44
Minhaj <mdminhajgdr@gmail.com>
2025-10-21 23:40:57 +0530

    feat: express endpoints

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,3 +1,10 @@
-import { greet } from "@monorepo/utils"
+import express from 'express'
+import { greet } from "@monorepo/shared"
 
-console.log(greet("World"))
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send(greet("World"));
+})
+
+app.listen("3000", () => {console.log(`Server started at port 3000`)})
